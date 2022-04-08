4965ba28d15753c96c35f4dcde22a0f32de8de3a
unknown <jerrytan7@gmail.com>
2022-04-08 00:00:05 -0400

    remove console.log

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.APP_PORT);
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
